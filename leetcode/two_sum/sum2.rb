d627a8d69e818e8fb2acf31eeb47ca1d90cf997f
kevn7n <kevn_nguyen@yahoo.com>
2022-04-05 22:56:58 -0600

    Start sum2 for time efficiency

---
+++ leetcode/two_sum/sum2.rb
@@ -0,0 +1,36 @@
+# @param {Integer[]} nums
+# @param {Integer} target
+# @return {Integer[]}
+
+def two_sum(nums, target)
+  hash = Hash.new
+  nums.each_with_index do |item,index|
+    diff = target - item
+    if nums.uniq.length == nums.length
+      if nums.include?(diff) && item != diff
+        x = nums.find_index(diff)
+        hash[index] = item
+        hash[x] = diff
+      end
+    elsif
+      if nums.include?(diff)
+        hash[index] = item
+        nums.delete_at(index)
+        # nums[index].pop
+        x = nums.find_index(diff) + 1
+        hash[x] = diff
+      end
+    end
+  end
+  p hash.keys
+  p nums
+end
+
+ary1 = [1,4,5]     # good
+ary2 = [3,3]       # good
+ary = [2,5,5,11]   # good
+tar = 10
+
+two_sum(ary,tar)
+
+## SUBMIT -- Time Limit Execeeded
