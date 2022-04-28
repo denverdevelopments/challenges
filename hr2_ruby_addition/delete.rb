ed0ff3c7c8652e4757c37d9b29ff7f655a6f00c2
kevn7n <kevn_nguyen@yahoo.com>
2022-04-28 01:51:16 -0600

    Finish

--- hr2_ruby_addition/delete.rb
+++ hr2_ruby_addition/delete.rb
@@ -1,22 +1,22 @@
 
 def end_arr_delete(arr)
     # delete the element from the end of the array and return the deleted element
-    arr.pop
+    return arr.pop
 end
 
 def start_arr_delete(arr)
     # delete the element at the beginning of the array and return the deleted element
-    arr.shift
+    return arr.shift
 end
 
 def delete_at_arr(arr, index)
     # delete the element at the position #index
-    arr.delete_at(index)
-    arr
+    return arr.delete_at(index)
+    # arr
 end
 
 def delete_all(arr, val)
     # delete all the elements of the array where element = val
-    arr.delete(var)
-    arr
+    return arr.delete(val)
+    # return arr
 end
