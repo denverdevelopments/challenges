b501f4fe260ff10b5ed1b8baa5f5658dfbd9112b
kevn7n <kevn_nguyen@yahoo.com>
2022-04-28 01:08:27 -0600

    Did first run, err last

--- hr1_ruby_addition/final.rb
+++ hr1_ruby_addition/final.rb
@@ -1,17 +1,24 @@
 
-
 def end_arr_add(arr, element)
     # Add `element` to the end of the Array variable `arr` and return `arr`
+    , ush(element)
 end
 
 def begin_arr_add(arr, element)
-    # Add `element` to the beginning of the Array variable `arr` and return `arr`
+    # Add `element` to the beginning of the Array variable `arr`, return `arr`
+    arr.unshift(element)
 end
 
 def index_arr_add(arr, index, element)
-    # Add `element` at position `index` to the Array variable `arr` and return `arr`
+    # Add `element` at position `index` to the Array variable `arr`, return `arr`
+    arr.insert(index, element)
 end
 
-def index_arr_multiple_add(arr, index)
+def index_arr_multiple_add(arr, index, e1, e2)
     # add any two elements to the arr at the index
+    arr.insert(index, e1, e2)
 end
+## Wrong -- not needed
+
+
+end_arr_add(arr, element)
