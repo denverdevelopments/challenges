1beb921960a0d86ef80a4f2a8495efbb1199a0f2
kevn7n <kevn_nguyen@yahoo.com>
2022-04-29 00:06:01 -0600

    Finish, pass

--- hr4_ruby_hash/hash.rb
+++ hr4_ruby_hash/hash.rb
@@ -1,1 +1,9 @@
 # Initialize 3 variables here as explained in the problem statement
+
+empty_hash = Hash.new
+
+default_hash = Hash.new(1)
+
+hackerrank = Hash.new
+hackerrank["simmy"] = 100
+hackerrank["vivmbbs"] = 200
