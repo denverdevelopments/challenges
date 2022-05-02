7df0b70bb3f9839bcc3081757a6daafd517b6050
kevn7n <kevn_nguyen@yahoo.com>
2022-05-01 20:37:47 -0600

    Simplify indexes

--- int_opa_hr3/ball_collision.rb
+++ int_opa_hr3/ball_collision.rb
@@ -1,28 +1,29 @@
 def findRemainingBalls(dir, str)
   remain = []
-  ind1 = -1
+  index = -1
   size = dir.length
   big_ind = str.find_index(str.max)
 
 # add conditional, if all the direction values are 1, or -1, return a new array of incrementing integers from 0 to (n - 1)
 
+  # dir.each_with_index
   (size-1).times do
-    ind1 += 1
-    unless ind1 == 0 && dir[ind1] == -1
+    index += 1
+    unless index == 0 && dir[index] == -1
       remain << big_ind
 
       if big_ind == 1 &&
-        if  dir[ind1] == dir[big_ind]*-1
+        if  dir[index] == dir[big_ind]*-1
         end
       elsif big_ind == -1
 
       end
     end
-    if ind1 == 0 && dir[ind1] == -1
+    if index == 0 && dir[index] == -1
       remain << 0
       dir.shift
     end
-    if ind2 == (size-1) && dir[ind2] == 1
+    if (index + 1) == (size-1) && dir[(index + 1)] == 1
       remain << (size -1)
     end
   end
