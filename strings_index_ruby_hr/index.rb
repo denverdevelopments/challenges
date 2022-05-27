ac582dbb5c81c30a7a5a53c3423c1a3272f42a52
kevn7n <kevn_nguyen@yahoo.com>
2022-05-27 02:24:34 -0600

    Add float interpolation

--- strings_index_ruby_hr/index.rb
+++ strings_index_ruby_hr/index.rb
@@ -1,6 +1,8 @@
 
 def serial_average(str)
   avg = ((str[4,5].to_f + str[10,5].to_f)/2).to_s
-  str[4,11] = ' '
-  str.strip + sprintf('%.2f', avg)
+  # str[4,11] = ' '
+  # str.strip + sprintf('%.2f', avg)
+  str[0,4] + sprintf('%.2f', avg)
+  # "#{str[0,4]}" + "#{sprintf('%.2f', avg)}"
 end
