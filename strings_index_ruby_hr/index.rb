157642b924829d8c0feab83841a65e87bbdf9342
kevn7n <kevn_nguyen@yahoo.com>
2022-05-28 16:27:30 -0600

    Finish

--- strings_index_ruby_hr/index.rb
+++ strings_index_ruby_hr/index.rb
@@ -3,6 +3,11 @@ def serial_average(str)
   avg = ((str[4,5].to_f + str[10,5].to_f)/2).to_s
   # str[4,11] = ' '
   # str.strip + sprintf('%.2f', avg)
-  str[0,4] + sprintf('%.2f', avg)
+    # str[0,4] + sprintf('%.2f', avg)
   # "#{str[0,4]}" + "#{sprintf('%.2f', avg)}"
+
+  arr=str.split("-")
+  avg = (((arr[1].to_f) + (arr[2].to_f))/2).round(2)
+
+	return "#{arr[0]}" + "-" + "#{avg}"
 end
