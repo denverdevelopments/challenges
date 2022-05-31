5b12cc56aa642e6429a39fe11f1af70d6175b44d
kevn7n <kevn_nguyen@yahoo.com>
2022-05-31 00:09:10 -0600

    Refactor with .count

--- strings_iteration_ruby_hr/iteration.rb
+++ strings_iteration_ruby_hr/iteration.rb
@@ -1,7 +1,7 @@
 def count_multibyte_char(str)
-  tally = 0
-  str.each_char {|x| tally += 1 if x.bytesize > 1}
-  return tally
+  # tally = 0
+  # str.each_char {|x| tally += 1 if x.bytesize > 1}
+  # return tally
   # str.each_char.select { |c| c.bytesize > 1 }.count
-  # str.each_char.count{|x| x.bytesize > 1}
+  str.each_char.count{|x| x.bytesize > 1}  #takes longer
 end
