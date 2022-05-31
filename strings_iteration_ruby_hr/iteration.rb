693adf41e8913fbcb534713f5fa33299edcbc826
kevn7n <kevn_nguyen@yahoo.com>
2022-05-31 00:06:10 -0600

    Finish, use each_char

--- strings_iteration_ruby_hr/iteration.rb
+++ strings_iteration_ruby_hr/iteration.rb
@@ -1,6 +1,7 @@
 def count_multibyte_char(str)
   tally = 0
-  str.each_byte {|x| tally += 1 if x > 1}
+  str.each_char {|x| tally += 1 if x.bytesize > 1}
   return tally
   # str.each_char.select { |c| c.bytesize > 1 }.count
+  # str.each_char.count{|x| x.bytesize > 1}
 end
