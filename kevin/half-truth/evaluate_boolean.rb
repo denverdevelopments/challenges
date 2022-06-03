fa1c86d61cc56a67e7960e6e82dcf572ad293f04
kevn7n <kevn_nguyen@yahoo.com>
2022-06-02 18:29:51 -0600

    Initial, add Huntress quizes

---
+++ kevin/half-truth/evaluate_boolean.rb
@@ -0,0 +1,33 @@
+#1.  Read in each line
+# File.foreach("input.txt") { |line| p line.split }
+
+#2.  Find space separators, breakup each indiv. booleans
+# File.each do |line|
+
+File.foreach("input.txt") do |line|
+  all_booleans = line.splice(" ")
+  # all_booleans = line.split
+  total = all_booleans.size
+  actual = all_booleans.count("true")
+  if actual > 0 && actual < total
+    p true
+  else
+    p false
+  end
+end
+
+def booleans()
+  # yield
+end
+
+booleans() do
+  p "Hello World"
+end
+
+#3.  Find number of booleans on lines (num)
+
+#4.  See if there are any count of 'true' values
+
+#5.  If count greater than zero and less than number, return 'true'
+
+#6.  Else return 'false'
