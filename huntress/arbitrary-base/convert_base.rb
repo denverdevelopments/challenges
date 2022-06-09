f44679d0a1dcf4302e030f60ff4c5fb4667672ca
kevn7n <kevn_nguyen@yahoo.com>
2022-06-09 01:30:38 -0600

    Refine comments

--- huntress/arbitrary-base/convert_base.rb
+++ huntress/arbitrary-base/convert_base.rb
@@ -1,35 +1,41 @@
 #1  Read in each lines
 
-#2  Check if is remainder
+#2  Break up number and base
+
+#3  Set values for number and base, convert them fron string to integer
+
+#3  Note: divident / divisor = quotient, remainder.
+
+#4  Create string or array to contain quotient values and remainder, for end output.
+
+#5  Repeat loop until quotient = 0, divide quotient by base, storing quotients.
+
+#6  Output string/array container, last entries first (ie reversed), write to new file.
 
 File.foreach("input.txt") do |line|
   pair = line.split
   num = pair.first.to_i
   base = pair.last.to_i
-    # if num == 0
-    #   File.write("newput.txt", answer.reverse.join(' ')+"\n", mode: "a")
-    #   return
-    # end
-  # answer = []
-  answer = num == 0 ? [0] : []  #ternary
 
-  # answer = ""
+  # answer = [] # no good when num = 0
+  # answer = [0] if num == 0
+  answer = num == 0 ? [0] : []  #ternary for '0' case
+
   # remainder = num % base
     # answer << (num % base)
     # num = num / base
-  # answer = [0] if num == 0
 
  ## recursion, w/o reverse
 
   # while num > base do
   until num < 1 do
-    rem = num % base
-    answer << rem
+    quotient = num % base
+    answer << quotient
     num = num / base
     # remainder = rem
   end
   # answer << (num % base)
-  File.write("new_out.txt", answer.reverse.join(' ')+"#{true}"+"\n", mode: "a")
+  File.write("new_out.txt", answer.reverse.join(' ')+"\n", mode: "a")
   # p answer.reverse.join(' ')
   # print answer.reverse.map {|n| p}
 
