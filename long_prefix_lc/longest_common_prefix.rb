38bf8e35c3ddb0e80b8fadb4bf7815e657d55c1c
kevn7n <kevn_nguyen@yahoo.com>
2022-06-17 01:19:14 -0600

    Initial setup

---
+++ long_prefix_lc/longest_common_prefix.rb
@@ -0,0 +1,11 @@
+# @param {String[]} strs
+# @return {String}
+def longest_common_prefix(strs)
+
+  # return false if x < 0
+  if x.to_s == x.to_s.reverse
+    p true
+  else
+    p false
+  end
+end
