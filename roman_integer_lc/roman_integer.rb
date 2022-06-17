9ba4da61a57710d55541a457dfc9d4074f05d3a5
kevn7n <kevn_nguyen@yahoo.com>
2022-06-17 01:05:08 -0600

    Adjust count

--- roman_integer_lc/roman_integer.rb
+++ roman_integer_lc/roman_integer.rb
@@ -3,7 +3,7 @@
 def roman_to_int(s)
   s = s.delete(" ").upcase
   ary = s.chars  #or split("")
-  del2 = s.count('IV') + s.count("IX")
+  del2 = s.count('IV') + s.count("IX")      #wrong count
   del20 = s.count('XL') + s.count("XC")
   del200 = s.count('CD') + s.count("CM")
   add = ary.count("I")+ary.count("V")*5+ary.count("X")*10+ary.count("L")*50+ary.count("C")*100+ary.count("D")*500+ary.count("M")*1000
@@ -11,23 +11,12 @@ def roman_to_int(s)
   total = add - del2*2 - del20*20 - del200*200
 end
 
-n1 = "IXIVII"  #No-19
+n = "IXIVII"  #No-19
 n2 = "LVIII"  #Yes-58
-n = "MCMXCIV"  #No-2216
+n3 = "MCMXCIV"  #No-2216
 p roman_to_int(n)
 
-  # hash = Hash.new
-  # nums.each_with_index do |item,index|
-  #   diff = target - nums.pop
-  #   if nums.include?(diff)
-  #     hash[index] = item
-  #     hash[nums.find_index(diff)+1] = diff
-  #   end
-  # end
-  # p hash.keys
-  # p nums
 
-# p two_sum(ary,tar)
 
 #1.  Read string by letter form left to right (array.shift)
     # s.upcase
