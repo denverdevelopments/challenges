245c9d86f64989a6e898eefd2e0b885eba06b0ce
kevn7n <kevn_nguyen@yahoo.com>
2022-06-20 13:49:50 -0600

    Add more psuedo code, min_by length

--- long_prefix_lc/longest_common_prefix.rb
+++ long_prefix_lc/longest_common_prefix.rb
@@ -1,11 +1,19 @@
 # @param {String[]} strs
 # @return {String}
 def longest_common_prefix(strs)
+  strs.min_by #length
+  del2 = s.scan(/IV/).length + s.scan(/IX/).length
 
-  # return false if x < 0
-  if x.to_s == x.to_s.reverse
-    p true
-  else
-    p false
-  end
 end
+
+
+#1.  Find shortest string
+
+#2.  Check if first letter is same in other 2 words
+
+#3.  If first is true, check first two letters
+
+#4.  Repeat until false
+  # When different, return to previous string
+
+#5.  Stop after last array entry, return total
