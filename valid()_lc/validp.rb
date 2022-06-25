4ebd7c26edc1fb2527f12d26ca1677a9885b217a
kevn7n <kevn_nguyen@yahoo.com>
2022-06-25 17:26:07 -0600

    Initial commit, create base files

---
+++ valid()_lc/validp.rb
@@ -0,0 +1,32 @@
+# @param {String[]} strs
+# @return {String}
+
+# [x,y].min { |x,y| x.size <=> y.size }
+# str[1..-1]
+# short = strs.min_by(&:length)
+# while short.slice!(0) == strs[1].first
+
+def longest_common_prefix(strs)
+  ltr = 0
+  out = String.new
+  # out = ''
+  while strs.first[ltr] == strs.first[ltr] && strs.first[ltr] == strs.last[ltr]
+    # out.concat(strs.first[ltr])
+    out += strs.first[ltr]
+    ltr += 1
+  end
+  out   #= '' if out.nil? OR .to_s
+end
+
+
+#1.  Find shortest string
+    # by str min length
+
+#2.  Check if first letter is same in other 2 words
+
+#3.  If first is true, check first two letters
+
+#4.  Repeat until false
+  # When different, return to previous string
+
+#5.  Stop after last array entry, return total
