d0b8056c2a853d18a698130b5285b879a5038088
kevn7n <kevn_nguyen@yahoo.com>
2022-06-30 23:59:27 -0600

    Add psuedo code

--- valid()_lc/validp.rb
+++ valid()_lc/validp.rb
@@ -3,7 +3,7 @@
 
 def is_valid(s)
   return false if s.size % 2 == 1
-
+  # .
 end
 
 
@@ -20,8 +20,9 @@ end
 
 #2.  Find innermost bracket, by finding first end bracket
   # search string for ')]}',  str.index(/[)]}]/)
+  # smallest index for a closing bracket, or biggest index of open
 
-#3.  If first is true, check first two letters
+#3.
 
 #4.  Repeat until false
   # When different, return to previous string
