5e22abfd32017ae5e518e5abad1641a089bd9622
kevn7n <kevn_nguyen@yahoo.com>
2022-07-27 02:23:01 -0600

    Finish str filter

--- coderb_terws/str_filter.rb
+++ coderb_terws/str_filter.rb
@@ -3,3 +3,6 @@ require 'json'
 require 'uri'
 
 resp = {'name': 'Joe Schmoe', 'hobbies':['running','coding','painting']}
+ind_start = resp.index('run')
+ind_end = resp.index(']') - 2
+puts  resp[ind_start..ind_end].gsub(",", ", ")
