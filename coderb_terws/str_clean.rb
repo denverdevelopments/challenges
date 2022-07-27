73258cafab8130eb7aedd60fc1baf6d7977eccee
kevn7n <kevn_nguyen@yahoo.com>
2022-07-27 11:18:28 -0600

    Update if with case / when

--- coderb_terws/str_clean.rb
+++ coderb_terws/str_clean.rb
@@ -6,14 +6,25 @@ require 'uri'
 resp = {'name': 'Joe Schmoe', 'DOB':"", 'Home':"-";'hobbies':['running','coding','painting']; 'schools'; {'Mid';'Henry', 'High':'N/A', 'College';'CSM'}}
 
 resp.values.select |k,v| do
-  if v.class == String &&
+  case v.class
+  when String
     resp.delete!(k)
-  elsif v.class == Array
-
-  elsif v.class == Hash
-
+  when Array
+    resp.delete!(k)
+  when Hash
+    resp.delete!(k)
+  else
   end
 
+  # if v.class == String &&
+  #   resp.delete!(k)
+  # elsif v.class == Array
+  #   resp.delete!(k)
+  # elsif v.class == Hash
+  #   resp.delete!(k)
+  # end
 end
 
-# or CASE and WHERE 
+# apple_stock > 1 ? (puts 'eat') : "buy"
+# apple_stock > 1 ? :eat_apple : :buy_apple
+# or CASE and WHERE
