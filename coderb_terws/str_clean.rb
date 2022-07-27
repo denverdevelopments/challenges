43a25970f1272d6d46fee2122dfd300523882cd6
kevn7n <kevn_nguyen@yahoo.com>
2022-07-27 02:34:05 -0600

    Start str clean

--- coderb_terws/str_clean.rb
+++ coderb_terws/str_clean.rb
@@ -1,5 +1,19 @@
 # from GET request, needs JSON.parse(response)
+# delet key-value pairs where v = "", "-", "N/A"
 require 'json'
 require 'uri'
 
-resp = {'name': 'Joe Schmoe', 'hobbies':['running','coding','painting']}
+resp = {'name': 'Joe Schmoe', 'DOB':"", 'Home':"-";'hobbies':['running','coding','painting']; 'schools'; {'Mid';'Henry', 'High':'N/A', 'College';'CSM'}}
+
+resp.values.select |k,v| do
+  if v.class == String &&
+    resp.delete!(k)
+  elsif v.class == Array
+
+  elsif v.class == Hash
+
+  end
+
+end
+
+# or CASE and WHERE 
